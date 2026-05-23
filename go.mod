26d6a60b13088c95784ef6ae8a8d112fd82eb4ed
unight0 <peter.sukhov0@gmail.com>
2026-05-23 17:25:40 +0100

    Update

--- go.mod
+++
@@ -1,5 +0,0 @@
-module chat2go
-
-go 1.26.2
-
-require github.com/mattn/go-sqlite3 v1.14.44
--- makecert.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-openssl req -nodes -x509 -newkey rsa:4096 -keyout key.pem -out cert.pem -sha256 -days 365
