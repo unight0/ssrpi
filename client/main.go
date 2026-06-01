df3fb3a51a5688234a117e64ee737be4873e77f0
unight0 <peter.sukhov0@gmail.com>
2026-06-01 14:45:27 +0100

    Copyleft notices

--- client/main.go
+++ client/main.go
@@ -1,3 +1,21 @@
+/* ======================================================================
+ * Copyleft (C) 2026
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ * ======================================================================
+*/
+
 package main
 
 import (
@@ -707,7 +725,13 @@ func (m model) View() string {
 func (m model) viewLogin() string {
 	var b strings.Builder
 
-	b.WriteString("\n  " + titleStyle.Render("ssrpi") + "\n\n")
+	b.WriteString("\n  " + titleStyle.Render("ssrpi") + "\n" +
+		infoStyle.Render(
+		"  ssrpi-client Copyleft (C) 2026\n" +
+    	"  This program comes with ABSOLUTELY NO WARRANTY\n" +
+    	"  This is free software, and you are welcome to redistribute it\n" +
+    	"  under certain conditions; see LICENSE for details\n") + "\n\n",
+	)
 
 	for _, e := range m.loginEntries {
 		b.WriteString(e.render() + "\n")
@@ -767,7 +791,6 @@ func writeLine(rw *bufio.ReadWriter, s string) {
 }
 
 // Main
-
 func main() {
 	server := flag.String("server", "", "Server address (host:port)")
 	servpass := flag.String("servpass", "", "Server password")
--- serv/main.go
+++ serv/main.go
@@ -1,3 +1,22 @@
+/* ======================================================================
+ * Copyleft (C) 2026
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ * ======================================================================
+*/
+
+
 package main
 
 import (
@@ -758,7 +777,19 @@ func dbSetup() {
 	checkFatal(err, "db.Exec()")
 }
 
+func GNUHello() {
+    fmt.Printf("ssrpi-serv Copyleft (C) 2026\n" +
+    	"This program comes with ABSOLUTELY NO WARRANTY\n" +
+    	"This is free software, and you are welcome to redistribute it\n" +
+    	"under certain conditions; see LICENSE for details\n" + 
+    	"=============================================================\n",
+	)
+}
+
 func main() {
+	GNUHello()	
+
+
 	port := flag.String("port", "8666", "Port that the server will listen at")
 	dbPath := flag.String("db", "./users.db", "Filepath for user database")
 	motdFilepathPtr := flag.String("motd", "", "MOTD to display to users upon login")
